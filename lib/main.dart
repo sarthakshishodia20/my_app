4761305967d8f045803d3a3ec5397a2eb7f49091
Sarthak Shishodia <sarthak.shishodia@jungleworks.com>
2026-02-11 00:38:33 +0530

    basic-components

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page For Counter App'),
+      home: const MyHomePage(title: 'Swiggy Clone'),
     );
   }
 }
@@ -45,21 +45,126 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 4,
+        shadowColor: const Color.fromARGB(255, 165, 2, 2),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        title: Text(widget.title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black54)),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+            },
+          ),
+          IconButton(icon: const Icon(Icons.person),
+          onPressed: () {},),
+          IconButton(icon: const Icon(Icons.settings),
+          onPressed: () {},),
+        ],
+      bottom: PreferredSize(
+    preferredSize: const Size.fromHeight(1),
+    child: Container(
+      height: 2,
+      color: const Color.fromARGB(255, 165, 2, 2), 
+    ),
+  ),),
+      body: Padding(
+  padding: const EdgeInsets.only(top: 20),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        width: 80,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.green.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
             ),
           ],
+          image: const DecorationImage(
+            image: NetworkImage(
+              'https://cdn-icons-png.flaticon.com/512/1046/1046784.png',
+            ),
+            fit: BoxFit.fitHeight,
+          ),
         ),
       ),
+      const SizedBox(width: 16),
+      Container(
+        width: 80,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+          image: const DecorationImage(
+            image: NetworkImage(
+              'https://cdn-icons-png.flaticon.com/512/1046/1046784.png',
+            ),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+      const SizedBox(width: 16),
+      Container(
+        width: 80,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.orange.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+          image: const DecorationImage(
+            image: NetworkImage(
+              'https://cdn-icons-png.flaticon.com/512/1046/1046784.png',
+            ),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+      const SizedBox(width: 16),
+      Container(
+        width: 80,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.purple.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+          image: const DecorationImage(
+            image: NetworkImage(
+              'https://cdn-icons-png.flaticon.com/512/1046/1046784.png',
+            ),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+    ],
+  ),
+),
+
+
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
